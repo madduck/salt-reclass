3db13bde5196b733300a76934c47ffa16ff0b3b1
martin f. krafft <madduck@madduck.net>
2010-07-30 10:23:45 +0200

    rename fs to yaml_fs

--- storage/yaml_fs.py
+++ storage/yaml_fs.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# fs.py — class implementing filesystem-based node classification
+# yaml_fs.py — class implementing filesystem-based YAML node classification
 #
 # Copyright © 2010 martin f. krafft <madduck@madduck.net>
 # Released under the terms of the Artistic Licence 2.0
 #
-__name__ = 'fs.py'
-__description__ = 'class implementing filesystem-based node classification'
+__name__ = 'yaml_fs.py'
+__description__ = 'class implementing filesystem-based YAML node classification'
 __version__ = '2010.07.29.2255'
 __author__ = 'martin f. krafft <madduck@madduck.net>'
 __copyright__ = 'Copyright © 2010 ' + __author__
